25efe86f9f7e55b68f9ed29929cb969069cd672d
chaeyoung <rnldyal1027@naver.com>
2023-03-09 15:17:57 +0900

    첫 스프링 연습!!

--- src/main/java/kr/hs/study/Main.java
+++ src/main/java/kr/hs/study/Main.java
@@ -1,6 +1,13 @@
 package kr.hs.study;
 
+import kr.hs.study.beans.TestBean;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
+    ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("config.xml");
+    //설정파일(config.xml)을 로딩하고 실행하는 것
+
+    ctx.close();
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
